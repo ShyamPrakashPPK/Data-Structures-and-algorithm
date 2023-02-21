bf10c0270c73bf005bac12a4e62ee643f8e6374b
ShyamPrakashPPK <shyamprakash0019@gmail.com>
2023-02-21 19:11:04 +0530

    started tree (bst)

---
+++ Tree/binarySearchTree.js
@@ -0,0 +1,25 @@
+class Node{
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
+class binarySearchTree{
+    constructor() {
+        this.root = null;
+    }
+
+ 
+    isEmpty() {
+        return this.root === null;
+    }
+
+
+}
+
+
+const bst = new binarySearchTree()
+console.log("is it empty....?",bst.isEmpty());
