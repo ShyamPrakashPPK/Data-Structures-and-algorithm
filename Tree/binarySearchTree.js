c0c926f1a93fe3092fc6cb37b0b200358d3de833
ShyamPrakashPPK <shyamprakash0019@gmail.com>
2023-02-22 18:49:45 +0530

    tree (bst)

--- Tree/binarySearchTree.js
+++ Tree/binarySearchTree.js
@@ -1,4 +1,4 @@
-class Node{
+class Node {
     constructor(value) {
         this.value = value;
         this.left = null;
@@ -7,19 +7,47 @@ class Node{
 }
 
 
-class binarySearchTree{
+class binarySearchTree {
     constructor() {
         this.root = null;
     }
 
- 
+
     isEmpty() {
         return this.root === null;
     }
 
+    insert(value) {
+        let newNode = new Node(value)
+        if (this.isEmpty) {
+            this.root = newNode
+        } else {
+            this.insertNode(this.root, newNode)
+        }
+    }
+
+    insertNode(root, newNode) {
+        if (newNode < root.value) {
+            if (root.left === null) {
+                root.left = newNode
+            } else {
+                this.insertNode(root.left,newNode)
+            }
+        } else {
+            if (root.right === null) {
+                root.right=newNode
+            } else {
+                this.insertNode(root.right,newNode)
+            }
+        }
+    }
 
 }
 
 
 const bst = new binarySearchTree()
-console.log("is it empty....?",bst.isEmpty());
+console.log("is it empty....?", bst.isEmpty());
+
+bst.insert(10)
+bst.insert(5)
+bst.insert(15)
