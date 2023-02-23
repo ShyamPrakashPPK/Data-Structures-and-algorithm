6dc1dde6d2aa25d842db853d2e426d44e2ec714c
ShyamPrakashPPK <shyamprakash0019@gmail.com>
2023-02-23 18:50:59 +0530

    tree (bst)

--- Tree/binarySearchTree.js
+++ Tree/binarySearchTree.js
@@ -19,7 +19,7 @@ class binarySearchTree {
 
     insert(value) {
         let newNode = new Node(value)
-        if (this.isEmpty) {
+        if (this.isEmpty()) {
             this.root = newNode
         } else {
             this.insertNode(this.root, newNode)
@@ -27,7 +27,7 @@ class binarySearchTree {
     }
 
     insertNode(root, newNode) {
-        if (newNode < root.value) {
+        if (newNode.value < root.value) {
             if (root.left === null) {
                 root.left = newNode
             } else {
@@ -42,6 +42,44 @@ class binarySearchTree {
         }
     }
 
+    search(root, value) {
+        if (!root) {
+            return false
+        } else {
+            if (root.value === value) {
+                return true
+            } else if (value < root.value) {
+                return this.search(root.left,value)
+            } else {
+                return this.search(root.right,value)
+            }
+        }
+    }
+
+    preOrder(root){
+        if(root){
+            console.log(root.value);
+            this.preOrder(root.left)
+            this.preOrder(root.right)
+        }
+    }
+
+    inOrder(root) {
+        if (root) {
+            this.inOrder(root.left)
+            console.log(root.value);
+            this.inOrder(root.right)
+        }
+    }
+
+    postOrder(root){
+        if(root){
+            this.postOrder(root.left)
+            this.postOrder(root.right)
+            console.log(root.value);
+        }
+    }
+
 }
 
 
@@ -51,3 +89,14 @@ console.log("is it empty....?", bst.isEmpty());
 bst.insert(10)
 bst.insert(5)
 bst.insert(15)
+bst.insert(3)
+bst.insert(7)
+
+
+console.log(bst);
+
+console.log(bst.search(bst.root,10));
+console.log(bst.search(bst.root, 5));
+console.log(bst.search(bst.root, 15));
+
+bst.postOrder(bst.root)
