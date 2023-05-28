855441165c62f7622fa98add9ba712fc1ae46b97
Shyam Prakash <shyamprakash0019@gmail.com>
2023-05-28 15:16:59 +0530

    ..

--- PracticeGround.js
+++ PracticeGround.js
@@ -1,1 +1,32 @@
-                                  
+class Node{
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList{
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.tail = newNode;
+            this.head = newNode;
+        } else {
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.length += 1;
+        return this;
+    }
+
+
+    prepend(value) {
+        
+    }
+}
